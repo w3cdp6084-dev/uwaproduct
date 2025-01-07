2a01510138d849cb0a01291b759a53def54e9979
w3cdp6084-dev <yusukew3@gmail.com>
2025-01-07 10:41:26 +0900

    Footer add

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+      <footer className="fixed bottom-0 left-0 right-0 bg-black text-white py-4">
+        <div className="container mx-auto text-center">
+          <p className="text-sm">@yusukemori</p>
+        </div>
+      </footer>
+    )
+  }
+  
+  
